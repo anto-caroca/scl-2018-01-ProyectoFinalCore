643e07fa6c71c2c855eb92b7af4b259c9179ed48
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-07-29 18:07:47 -0400

    recibir upstream

--- login/login.js
+++ login/login.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            window.location = "panel.html";// aqui se debe redireccionar a hoja de login
+            location.href="";// aqui se debe redireccionar a hoja de login
         }
     });
 }
